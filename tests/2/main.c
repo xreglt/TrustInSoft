b53b4af0f1e19f08b2310ea4cf57de2712af8a93
RundongZhou <rundongzhou@gmail.com>
2018-04-06 17:20:34 -0700

    u

--- tests/2/main.c
+++ tests/2/main.c
@@ -11,7 +11,8 @@ int bug() {
 
 
 int main(int argc, char **argv) {
-	int counter = tis_interval(0, 10);;
+	// int counter = tis_interval(0, 10);;
+	int counter = tis_nondet (17, 42);
 	// tis_scanf("%d", &counter);
 
 	if (counter == 75) {
